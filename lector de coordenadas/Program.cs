e63b2d36a94d95e8f5d263754e69066d687c1c0d
Lukatis16 <lucasr06@ucm.es>
2024-02-29 20:29:31 +0100

    commit inicial

---
+++ lector de coordenadas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace lector
+{
+    class lectordecoordenadas
+    {
+        static void Main()
+        {
+            Console.WriteLine("Inserte las coordenadas con el formato \"(x,y)\", por favor");
+            string coordenadas = Console.ReadLine();
+            coordenadas = coordenadas.Remove(0, 1);
+            coordenadas = coordenadas.Remove(3, 1);
+            string[] coor = coordenadas.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            int[] coors = new int[2];
+            for(int i = 0; i < coors.Length; i++)
+            {
+                coors[i] = int.Parse(coor[i]);
+            }
+            Console.WriteLine(coor[0] + "x " + coor[1] + "y");
+        }
+    }
+}
